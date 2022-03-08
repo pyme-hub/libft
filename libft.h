1416e0ef88c0263955bc25edc22f89d4c2da9dc1
Kulastree <kulastreepim@gmail.com>
2022-03-08 18:43:37 +0700

    ..

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kkongim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/05 17:56:07 by kkongim           #+#    #+#             */
-/*   Updated: 2022/03/05 17:56:25 by kkongim          ###   ########.fr       */
+/*   Updated: 2022/03/08 18:39:57 by kkongim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *s);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strnstr(const char *str, const char *to_find, size_t len);
 char	*ft_strrchr(const char *str, int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
