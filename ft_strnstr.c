24c3bc78a93f12f45d4561024037d4520bc93b1d
Kulastree <kulastreepim@gmail.com>
2022-03-08 15:12:04 +0700

    ..

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,10 +6,30 @@
 /*   By: kkongim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 20:07:46 by kkongim           #+#    #+#             */
-/*   Updated: 2022/03/05 17:53:06 by kkongim          ###   ########.fr       */
+/*   Updated: 2022/03/08 15:02:12 by kkongim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strnstr(const char *str, const char *to_find, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (*to_find == '\0')
+		return ((char *) str);
+	while (str[i] != '\0' && i < len)
+	{
+		j = 0;
+		while (str[i + j] == to_find[j] && i + j < len)
+		{
+			j++;
+			if (to_find[j] == '\0')
+				return ((char *)&str[i]);
+		}
+		i++;
+	}
+	return (0);
+}
